7c876350a49023aed489ce4c1017967728700da3
Bill <h853047455@gmail.com>
2021-05-06 11:29:06 +0800

    [Add] 新增可簡化程式碼的註解

--- app/src/main/java/com/example/mvvm_demo_androidx/RecyclerViewDemo/MoodListAdapter.kt
+++ app/src/main/java/com/example/mvvm_demo_androidx/RecyclerViewDemo/MoodListAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mvvm_demo_androidx.R
 import com.example.mvvm_demo_androidx.database.ListData
+import kotlinx.android.synthetic.main.content_mood_list.view.*
 
 class MoodListAdapter(private val clickListener: ItemClickListener) : RecyclerView.Adapter<MoodListAdapter.ViewHolder>() {
 
@@ -29,10 +30,12 @@ class MoodListAdapter(private val clickListener: ItemClickListener) : RecyclerVi
     override fun getItemCount() = data?.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        val img_mood: ImageView = itemView.findViewById(R.id.quality_image)
-        val txv_title: TextView = itemView.findViewById(R.id.txv_description)
-
+        
+        /**
+         * 方法一：
+         */
+        val img_mood: ImageView = itemView.findViewById(R.id.quality_image) //itemView.quality_image
+        val txv_title: TextView = itemView.findViewById(R.id.txv_description) //itemView.txv_description 可以直接替換成這個
         fun bind(item: ListData,clickListener:ItemClickListener) {
             img_mood.setImageResource(
                 when (item.moodIndex) {
@@ -50,7 +53,30 @@ class MoodListAdapter(private val clickListener: ItemClickListener) : RecyclerVi
                 clickListener.onClick(item.time.toString())
             }
         }
+        /**
+         * 方法二：
+         */
+        fun bind2(item: ListData,clickListener:ItemClickListener) {
+            itemView.quality_image.setImageResource(
+                when (item.moodIndex) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.ic_sleep_0
+                }
+            )
+            itemView.txv_description.text = item.time.toString()
+            itemView.setOnClickListener {
+                clickListener.onClick(item.time.toString())
+            }
+        }
 
+        /**
+         * 綁定Layout
+         */
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
